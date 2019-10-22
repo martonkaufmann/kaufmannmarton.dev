c7c6eee483f180360dfaeaac6ad816107214b87c
Marton Kaufmann <kaufmann.marton@gmail.com>
2019-10-22 12:20:05 +0200

    Run certain postprocessors on production only

--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,21 @@
 module.exports = {
     plugins: [
         require('tailwindcss'),
-        require('@fullhuman/postcss-purgecss')({
-            content: [
-                './src/**/*.html',
-                './src/**/*.js',
-                './src/**/*.jsx',
-            ],
+        ...process.env.NODE_ENV === 'production' ? [
+            require('@fullhuman/postcss-purgecss')({
+                content: [
+                    './src/**/*.html',
+                    './src/**/*.js',
+                    './src/**/*.jsx',
+                ],
 
-            // Include any special characters you're using in this regular expression
-            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
-        }),
-        require('autoprefixer'),
-        require('cssnano')({
-            preset: 'default',
-        }),
+                // Include any special characters you're using in this regular expression
+                defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+            }),
+            require('autoprefixer'),
+            require('cssnano')({
+                preset: 'default',
+            }),
+        ] : [],
     ]
 }
