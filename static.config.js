76fc723fa5a12435b9fbc8aedc2f5c7c5523396d
Marton Kaufmann <kaufmann.marton@gmail.com>
2019-10-22 22:39:50 +0200

    Comment out fetching of remote data

--- static.config.js
+++ static.config.js
@@ -3,24 +3,24 @@ import axios from 'axios'
 
 export default {
   getRoutes: async () => {
-    const { data: posts } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts'
-    )
+    // const { data: posts } = await axios.get(
+    //   'https://jsonplaceholder.typicode.com/posts'
+    // )
 
     return [
-      {
-        path: '/blog',
-        getData: () => ({
-          posts,
-        }),
-        children: posts.map(post => ({
-          path: `/post/${post.id}`,
-          template: 'src/containers/Post',
-          getData: () => ({
-            post,
-          }),
-        })),
-      },
+    //   {
+    //     path: '/blog',
+    //     getData: () => ({
+    //       posts,
+    //     }),
+    //     children: posts.map(post => ({
+    //       path: `/post/${post.id}`,
+    //       template: 'src/containers/Post',
+    //       getData: () => ({
+    //         post,
+    //       }),
+    //     })),
+    //   },
     ]
   },
   plugins: [
