6bf8de98b1895bbf7b90e26581b86663afa33605
Marton Kaufmann <kaufmann.marton@gmail.com>
2019-10-22 12:14:01 +0200

    Add unused css purging

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,16 @@
 module.exports = {
     plugins: [
         require('tailwindcss'),
+        require('@fullhuman/postcss-purgecss')({
+            content: [
+                './src/**/*.html',
+                './src/**/*.js',
+                './src/**/*.jsx',
+            ],
+
+            // Include any special characters you're using in this regular expression
+            defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+        }),
         require('autoprefixer'),
         require('cssnano')({
             preset: 'default',
