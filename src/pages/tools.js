602473ce3b9201774330f6fbf767b4208738faac
Marton-Kaufmann <marton.kaufmann@ibm.com>
2019-10-25 10:29:48 +0200

    Add space between tool name and description

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -6,7 +6,7 @@ export default () => {
 
     return <ul className="my-4 mx-auto px-2 w-10/12 md:w-1/2 lg:md-1/3">
         {tools.map(tool => <li className="mb-8" key={`tool-${tool.id}`}>
-            <section className="flex items-end">
+            <section className="flex items-end mb-1">
                 <img src={tool.image} className="h-8 mr-2" />
                 <a href={tool.url} target="_blank" className="text-xl underline">
                     {tool.name}
