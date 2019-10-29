31bece7f28224201166e3fbe2591fbac36f67176
Marton-Kaufmann <marton.kaufmann@ibm.com>
2019-10-29 12:35:23 +0100

    Add alt text for tool image

--- src/pages/tools.js
+++ src/pages/tools.js
@@ -8,7 +8,7 @@ export default () => {
     return <ImageLazyLoad><ul className="my-4 mx-auto px-2 w-10/12 md:w-1/2 lg:md-1/3">
         {tools.map(tool => <li className="mb-8" key={`tool-${tool.id}`}>
             <section className="flex items-end mb-1">
-                <img data-src={tool.image} className="h-8 mr-2" />
+                <img data-src={tool.image} alt={tool.name} className="h-8 mr-2" />
                 <a href={tool.url} target="_blank" rel="noreferrer noopener" className="text-xl underline">
                     {tool.name}
                 </a>
